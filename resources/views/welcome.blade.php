6f8cce7753ab2ab3b1d5de84c9d67d29367aa7e6
Giuseppe-Calabrese <giuseppecalabrese03@gmail.com>
2023-06-14 11:05:13 +0200

    scelta palette e inizio idee header

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,7 +2,16 @@
     <div class="container text-center">
         <div class="row justify-content-center">
             <div class="col-12">
-                <h1 class="display-1">Calabrian.it</h1>
+                <h1 class="display-1 passion-one">INDOSSAMI<span class="text-primaryC">.it</span></h1>
+            </div>
+        </div>
+    </div>
+    <div class="container-fluid justify-content-between">
+        <div class="row justify-content-center">
+            <div class="col-12 d-flex flex-row justify-content-between">
+                <img class="foto-header" src="immagini/foto-modelli1.jpg" alt=""> 
+                <img class="foto-header" src="immagini/foto-modelli2.jpg" alt=""> 
+                <img class="foto-header" src="immagini/foto-modelli3.jpg" alt=""> 
             </div>
         </div>
     </div>
